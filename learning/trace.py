aa1ac6c8320b81aeb3d8398a574eeb25f4302920
Random-W <33356939+Random-W@users.noreply.github.com>
2017-11-11 21:09:41 -0500

    Add files via upload

--- learning/trace.py
+++ learning/trace.py
@@ -4,24 +4,24 @@ cap=cv2.VideoCapture("2.mp4")
 kernel1 = np.ones((2,2),np.uint8)
 kernel2 = np.ones((2,2),np.uint8) 
 while(1):
-    # 获取每一帧
+    # get every frame
     ret,frame=cap.read()
-    # 转换到HSV
+    # convert into HSV
     hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-    # 设定黄色的阈值
+    # set yellow boundaries
     lower_yellow=np.array([10,100,100])
     upper_yellow=np.array([175,255,255])
-    # 设定白色的阈值
-    lower_white=np.array([15,15,150])
-    upper_white=np.array([100,255,255])    
-    # 根据阈值构建掩模   
+    # set white boundaries
+    lower_white=np.array([0,50,175])
+    upper_white=np.array([50,100,255])    
+    # calculate mask   
     mask1=cv2.inRange(hsv,lower_yellow,upper_yellow)
     mask2=cv2.inRange(hsv,lower_white,upper_white)
     
     #mask1 = cv2.erode(mask1,kernel2,iterations = 3)
     #mask = cv2.dilate(mask,kernel1,iterations = 2)
     
-    # 对原图像和掩模进行位运算
+    # mask
     res1=cv2.bitwise_and(frame,frame,mask=mask1)
     res2=cv2.bitwise_and(frame,frame,mask=mask2)
     #resize
@@ -29,7 +29,7 @@ while(1):
     res1 = cv2.resize(res1,(400,300))
     res2 = cv2.resize(res2,(400,300))    
     img = np.hstack((frame,res1,res2))
-    # 显示图像
+    # show
     #cv2.imshow('frame',frame)
     #cv2.imshow('mask',mask)
     #cv2.imshow('res',res)
@@ -37,5 +37,5 @@ while(1):
     k=cv2.waitKey(5)&0xFF
     if k==27:
         break
-    # 关闭窗口
+    # close windows
 cv2.destroyAllWindows()
