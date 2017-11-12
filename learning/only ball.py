3fba6493dd0e5fe8039c4d237318271149921573
Random-W <33356939+Random-W@users.noreply.github.com>
2017-11-12 00:13:23 -0500

    Add files via upload

---
+++ learning/only ball.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy as np
+from time import sleep
+cap=cv2.VideoCapture("2.mp4")
+kernel1 = np.ones((1,2),np.uint8)
+
+while(1):
+    # get every frame
+    ret,frame=cap.read()
+    #frame = cv2.resize(frame,(640,360))
+    # convert into HSV
+    hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    # set yellow boundaries
+    lower_yellow=np.array([15,200,240])
+    upper_yellow=np.array([255,250,255])
+    # set white boundaries
+    lower_white=np.array([15,50,100])
+    upper_white=np.array([100,100,255])     
+    # set white center line
+    lower_white2=np.array([0,0,215])
+    upper_white2=np.array([200,18,255])    
+    # calculate mask
+    #ball
+    mask1=cv2.inRange(hsv,lower_yellow,upper_yellow)
+    #white boundaries
+    mask2=cv2.inRange(hsv,lower_white,upper_white)
+    #white center line
+    mask3=cv2.inRange(hsv,lower_white2,upper_white2)
+    mask2 = cv2.dilate(mask2,kernel1,iterations = 2)
+    mask3 = cv2.dilate(mask3,kernel1,iterations = 2)
+    
+    #total 
+    #mask1 = cv2.bitwise_or(mask1,mask2)
+    #mask1 = cv2.bitwise_or(mask1,mask3)
+    # mask
+    #res1=cv2.bitwise_and(frame,frame,mask=mask1)
+    # show
+    cv2.imshow('compare', mask1)
+    #sleep(0.1)
+    k=cv2.waitKey(1)&0xFF
+    if k==27:
+        break
+    # close windows
+cv2.destroyAllWindows()
